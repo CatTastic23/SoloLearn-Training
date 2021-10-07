6d2fd46f817e84740102540dd4338609a5becbce
Catherine Nguyen <catherinenguyen@csus.edu>
2021-10-07 13:39:29 -0700

    Added code

--- Data Science/Data Manipulation/AverageOfRows.py
+++ Data Science/Data Manipulation/AverageOfRows.py
@@ -1,6 +1,17 @@
 # Data Manipulation: Code Project
 # Data Science - Average of Rows
 import numpy as np
-n, p = [int(x) for x in input().split()]
-print(n)
+rows, cols = [int(x) for x in input().split()]
+#list = [[1 for cols in range(cols)] for rows in range(rows)]
 
+list=[]
+for i in range(rows):
+    list.append([float(y) for y in input().split()])
+print(list)
+
+#for rows in range(rows):
+#    for cols in range(cols):
+#        list[rows][cols]= float(input())
+#print(list)
+avg = np.mean(list, axis=1)
+print(avg)
