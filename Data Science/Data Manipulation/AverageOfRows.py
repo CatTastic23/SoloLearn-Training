a9467dcfc338495c0219dbcf3efe60d1ea8c6e5b
Catherine Nguyen <catherinenguyen@csus.edu>
2021-10-08 12:58:30 -0700

    I removed my test code. So that the test cases could pass.

--- Data Science/Data Manipulation/AverageOfRows.py
+++ Data Science/Data Manipulation/AverageOfRows.py
@@ -6,7 +6,7 @@ rows, cols = [int(x) for x in input().split()]
 list=[]
 for i in range(rows):
     list.append([float(y) for y in input().split()[:cols]])
-print(list)
+# print(list) Test code to check matrix ouput.
 
 avg = np.mean(list, axis=1)
 print(avg)
