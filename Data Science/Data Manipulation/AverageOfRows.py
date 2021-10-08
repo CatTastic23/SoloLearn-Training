8ce43a803b97a6bae247762d6674d7ade613c371
Catherine Nguyen <catherinenguyen@csus.edu>
2021-10-08 13:16:08 -0700

    Same sigfig error fix.

--- Data Science/Data Manipulation/AverageOfRows.py
+++ Data Science/Data Manipulation/AverageOfRows.py
@@ -2,11 +2,8 @@
 # Data Science - Average of Rows
 import numpy as np
 rows, cols = [int(x) for x in input().split()]
-
 list=[]
 for i in range(rows):
     list.append([float(y) for y in input().split()[:cols]])
-
 avg = np.array(list).mean(axis=1).round(2)
 print(avg)
-#[float(y) for y in input().split()[:cols]]
