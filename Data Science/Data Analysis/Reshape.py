26d9fb1d710d6a4ff146587e89dbf021c3e99868
Catherine Nguyen <catherinenguyen@csus.edu>
2021-10-13 14:27:33 -0700

    Added round() into the code to ensure 2 decimals.

--- Data Science/Data Analysis/Reshape.py
+++ Data Science/Data Analysis/Reshape.py
@@ -1,7 +1,7 @@
 import numpy as np
 r = int(input())
 lst = [float(x) for x in input().split()]
-arr = np.array(lst)
+arr = np.array(lst).round(2)
 div = int(len(arr)/r)
 reshape = arr.reshape(r, div)
 print(reshape)
