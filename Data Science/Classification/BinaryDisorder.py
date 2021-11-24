593b87f73673cba38d751c4f154aa43890a679ce
Catherine Nguyen <catherinenguyen@csus.edu>
2021-11-24 12:07:59 -0800

    solved binary disorder

--- Data Science/Classification/BinaryDisorder.py
+++ Data Science/Classification/BinaryDisorder.py
@@ -1,2 +1,15 @@
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
 y_true = [int(x) for x in input().split()]
 y_pred =  [int(x) for x in input().split()]
+
+y_true = np.array(y_true)
+y_true = y_true.astype(str)
+y_pred = np.array(y_pred)
+y_pred = y_pred.astype(str)
+
+
+new_data = confusion_matrix(y_pred, y_true, labels=['1','0'])
+new_data = new_data.astype('f')
+print(new_data)
